f9b513a5298537a1001a69324add65a3094911f0
rafa-oliveira06 <rafa.oliveira1432@gmail.com>
2021-03-31 21:59:58 -0300

    commit

--- lib/components/my_container.dart
+++ lib/components/my_container.dart
@@ -7,23 +7,28 @@ class MyContaier extends StatelessWidget {
   const MyContaier({Key key, this.tittle, this.colour, this.ontap}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: ontap,
-          child: Container(
-            height: MediaQuery.of(context).size.height*.5,
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.all( 32),
-            decoration: BoxDecoration(
-              color: colour,
-              borderRadius: BorderRadius.circular(30)
-            ),
-            child: Center(child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Text(tittle,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 22, color: Colors.white),),
-            )),
-        
+    return Center(
+      child: GestureDetector(
+        onTap: ontap,
+            child: Container(
+              height: MediaQuery.of(context).size.height*.6,
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.all( 32),
+              decoration: BoxDecoration(
+                color: colour,
+                borderRadius: BorderRadius.circular(30)
+              ),
+              child: Center(child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Text(tittle,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22,
+                 color: Colors.white,
+                  fontWeight: FontWeight.bold ),
+                  ),
+              )),
+          
+        ),
       ),
     );
   }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,11 @@ import 'package:my_app/components/my_container.dart';
 import 'dart:math';
 
 import 'package:my_app/data/data.dart';
+import 'package:random_color/random_color.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share/share.dart';
+
+RandomColor _randomColor=RandomColor();
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -13,29 +18,50 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String versebutton='';
   int currentindex;
+  Color _color=_randomColor.randomColor();
+  final _screenShotControler=ScreenshotController();
+
   void gerarversiculo(){
     currentindex=Random().nextInt(versiculos.length);
+    _color=_randomColor.randomColor(
+    colorBrightness: ColorBrightness.dark,
+    colorSaturation: ColorSaturation.lowSaturation);
     setState(() {
       versebutton=versiculos[currentindex];
     });
   }
+
+
   @override
   void initState() { 
     super.initState();
     gerarversiculo();
   }
+
+  void _takeScreenShot()async{
+  final imageFile =await _screenShotControler.capture();
+  Share.shareFiles([imageFile.path],
+  text: 'compartilhado por Rafaela');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: Text('caixinha de promessas'),
     centerTitle: true,
+    backgroundColor: Color(0xff31313a),
     actions: [
-      IconButton(onPressed: (){}, icon: Icon(Icons.share))
+      IconButton(onPressed: (){
+        _takeScreenShot();
+      }, icon: Icon(Icons.share))
     ],),
-    body: MyContaier(colour: Colors.red,
-    tittle: versebutton,
-    ontap: (){
-      gerarversiculo();
-    },),
+    backgroundColor: Color(0xff837c7b),
+    body: Screenshot(controller: _screenShotControler,
+          child: MyContaier(colour: _color,
+      tittle: versebutton,
+      ontap: (){
+        gerarversiculo();
+      },),
+    ),
     
       
     );
